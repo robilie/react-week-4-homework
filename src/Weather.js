5c8604a831d2de8ffaae0085c5fe3b130935cfda
robilie <obilie.roberta@gmail.com>
2021-10-30 20:28:27 -0700

    Added weather component and styling for css file

---
+++ src/Weather.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./Weather.css";
+
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div id="recBox">
+          <div className="row">
+            <div className="col-sm-4 daily-temp">
+              <h1>21°C</h1>
+            </div>
+            <div className="col-sm-4 daily-temp">
+              <div className="high-low">
+                <sup id="high">25</sup>/<sub id="low">18</sub>
+              </div>
+              <div className="alter-ego">
+                <sup>
+                  <a href="/" id="celsius-link" className="temp-link">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="/" id="fahrenheit-link" className="temp-link">
+                    °F
+                  </a>
+                </sup>
+              </div>
+            </div>
+            <div className="col-sm-4 daily-temp">
+              <img
+                id="weather-image-today"
+                src="https://www.freeiconspng.com/uploads/cloud-sun-weather-icon-32.png"
+                className="weather-size"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <h6 className="date-time">Mon October 11, 2:00pm</h6>
+            </div>
+            <div className="col-6">
+              <h6 className="feels-like" id="feels-like-temp">
+                Feels like 25°
+              </h6>
+            </div>
+          </div>
+          <h2>White Rock</h2>
+
+          <h3>Partly cloudy skies</h3>
+
+          <br />
+          <h4>DETAIL</h4>
+          <div className="row">
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/219/219816.png"
+                  class="card-img-top"
+                  alt="humidity"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Humidity
+                    <br />
+                    <span id="humidity">42 </span>%
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/959/959711.png"
+                  className="card-img-top"
+                  alt="wind"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Wind
+                    <br />
+                    <span id="wind-speed"> 1</span>
+                    <sub>km/hr </sub>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+}
